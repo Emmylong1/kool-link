d6a993063812ad64831d57d5fb91cfcd90608dd2
Ot011 <ntitiemmanuel@gmail.com>
2026-02-23 17:03:16 +0100

    logo change

--- src/sections/HeaderHero.jsx
+++ src/sections/HeaderHero.jsx
@@ -12,7 +12,7 @@ export default function HeaderHero() {
             {/* LOGO PLACEHOLDER */}
             <div className="  overflow-hidden w-50 h-10 flex items-center justify-center">
               
-                  <img src="/logo.png" alt="Cool Flow logo" className="w-full h-full object-cover" />
+                  <img src="/logoo.png" alt="Cool Flow logo" className="w-full h-full object-cover" />
              
            
             </div>
@@ -64,7 +64,7 @@ export default function HeaderHero() {
               <div className="flex items-center gap-3 mb-4">
                     <div className="  overflow-hidden w-50 h-10 flex items-center justify-center">
               
-                  <img src="/logo.png" alt="Cool Flow logo" className="w-full h-full object-cover" />
+                  <img src="/logoo.png" alt="Cool Flow logo" className="w-full h-full object-cover" />
              
            
             </div>
